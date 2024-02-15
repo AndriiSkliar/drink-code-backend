860947e1cfd5d17d77209ef6e5f9a921358eda79
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-15 08:21:11 +0200

    init project

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-
-const { DB_HOST, PORT = 3000 } = process.env;
+const DB_HOST =
+  "mongodb+srv://Team:PArgC8VFB66hSuXM@drinkcode.d6eetf3.mongodb.net/cocktails_creator?retryWrites=true&w=majority";
+const PORT = 3000;
+// const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
