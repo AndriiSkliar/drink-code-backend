5aeb313a7018c53f9e120d70d12e1e78c72b0872
AntKhom <antonhomenko1988@gmail.com>
2024-02-15 09:45:37 -0500

    app-route-auth-users

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRouter = require("./routes/api/users");
-const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
 
 const app = express();
 
@@ -13,9 +13,10 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/api/users", authRouter);
-app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
