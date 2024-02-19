ac507c8db738ab22a5a72afc7055de512ad20fab
maershaa <valeria.work2022@gmail.com>
2024-02-19 19:35:11 +0200

    addden controller getHomePageDrinks and GET/mainpage route
    реализован маршрут GET/mainpage Отримання коктейлів для головної сторінки
    
    делала контроллер getHomePageDrinks

---
+++ controllers/drinks/getHomePageDrinks.js
@@ -0,0 +1,27 @@
+const { Drink } = require("../../models/drink"); 
+const { ctrlWrapper } = require("../../helpers");
+
+
+const getHomePageDrinks = async (req, res, next) => {
+  try {
+    const { isAdult } = req.user;
+    // console.log('isAdult:', isAdult)
+    // console.log(' req.user:',  req.user)
+
+    let drinks;
+
+    if (isAdult !== true) {
+      drinks = await Drink.find({ alcoholic: "Non alcoholic" }).sort({
+        createdAt: -1,  //чтобы отображались последние добавленные
+        _id: -1, // если добавлены одномоментно то по убиванію id отобразятся
+      });
+    } else {
+      drinks = await Drink.find().sort({ createdAt: -1, _id: -1 });
+    }
+
+    res.json(drinks);
+  } catch (error) {
+    next(error);
+  }
+}
+module.exports =    ctrlWrapper(getHomePageDrinks)
