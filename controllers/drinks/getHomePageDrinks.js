53886dbda6697e5ab15bc82331237ca42a495682
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-19 21:14:00 +0200

    delete try...catch

--- controllers/drinks/getHomePageDrinks.js
+++ controllers/drinks/getHomePageDrinks.js
@@ -1,27 +1,21 @@
-const { Drink } = require("../../models/drink"); 
+const { Drink } = require("../../models/drink");
 const { ctrlWrapper } = require("../../helpers");
 
-
 const getHomePageDrinks = async (req, res, next) => {
-  try {
-    const { isAdult } = req.user;
-    // console.log('isAdult:', isAdult)
-    // console.log(' req.user:',  req.user)
+  const { isAdult } = req.user;
 
-    let drinks;
+  let drinks;
 
-    if (isAdult !== true) {
-      drinks = await Drink.find({ alcoholic: "Non alcoholic" }).sort({
-        createdAt: -1,  //чтобы отображались последние добавленные
-        _id: -1, // если добавлены одномоментно то по убиванію id отобразятся
-      });
-    } else {
-      drinks = await Drink.find().sort({ createdAt: -1, _id: -1 });
-    }
-
-    res.json(drinks);
-  } catch (error) {
-    next(error);
+  if (isAdult !== true) {
+    drinks = await Drink.find({ alcoholic: "Non alcoholic" }).sort({
+      createdAt: -1, // чтобы отображались последние добавленные
+      _id: -1, // если добавлены одномоментно то по убиванію id отобразятся
+    });
+  } else {
+    drinks = await Drink.find().sort({ createdAt: -1, _id: -1 });
   }
-}
-module.exports =    ctrlWrapper(getHomePageDrinks)
+
+  res.json(drinks);
+};
+
+module.exports = ctrlWrapper(getHomePageDrinks);
