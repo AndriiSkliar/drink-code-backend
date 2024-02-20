adee473b98bcc170da3fa375e7d7c0dd9d0ff33e
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-20 10:11:47 +0200

    changed the lines

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -8,25 +8,29 @@ const addDrink = require("../../controllers/drinks/addDrink");
 const ctrlAll = require("../../controllers/drinks/getAllDrinks");
 const filter = require("../../controllers/filters/filters");
 
-
 const { isValidId, authenticate } = require("../../middlewares");
 
 // const { schemas } = require("../../models/drink");
 
 const router = express.Router();
 
-router.get("/mainpage", authenticate, getHomePageDrinks); // GET/mainpage Отримання коктейлів для головної сторінки
+// GET/mainpage Отримання коктейлів для головної сторінки
+router.get("/mainpage", authenticate, getHomePageDrinks);
+
 // GET/popular Отримання популярних  коктейлів
+
 // GET/search Отримання коктейлів по категорії + інгредієнту + ключовому слову
-router.get('/', authenticate, ctrlAll.getAllDrinks)
-router.get('/search', authenticate, filter.getDrinks)
-router.get('/search/category', authenticate, filter.getDrinksByCategory)
-router.get('/search/ingredients', authenticate, filter.getDrinksByIngredient)
+router.get("/", authenticate, ctrlAll.getAllDrinks);
+router.get("/search", authenticate, filter.getDrinks);
+router.get("/search/category", authenticate, filter.getDrinksByCategory);
+router.get("/search/ingredients", authenticate, filter.getDrinksByIngredient);
+
 // GET/:id Отримання одного коктейлю за ID
 router.get("/:id", authenticate, isValidId, ctrl.getById);
 
 // POST/own/ add Додавання власного коктейлю
 router.post("/own", jsonParser, addDrink);
+
 // DELETE/own/remove Видалення власного коктейлю
 // GET/own Отримання власних коктейлів
 // POST/favorite/add/ Додавання коктейлю до обраних
