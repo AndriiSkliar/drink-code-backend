29adc8bfc7792e3c01658fcf761bde8b77c92a34
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-21 11:25:22 +0200

    del index.js by controllers

--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-const userControllers = require("./auth");
-const filtersControllers = require("./filters");
-const drinksControllers = require("./drinks");
-
-module.exports = { userControllers, filtersControllers, drinksControllers };
