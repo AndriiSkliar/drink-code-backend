6bcc7f9dea4b3073fe10683035c6c2ec5afa6785
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-28 04:34:13 +0200

    upd

--- controllers/filters/getDrinksByFilters.js
+++ controllers/filters/getDrinksByFilters.js
@@ -2,8 +2,9 @@ const { Drink } = require("../../models/drink");
 const { fullYearsCount, HttpError } = require("../../helpers");
 
 const getDrinksByFilters = async (req, res) => {
-  const { category, ingredient, drink } = req.body;
+  const { category, ingredient, drink, page = 1, limit = 8 } = req.query;
   const { birthday } = req.user;
+  const skip = (page - 1) * limit;
   const fullYears = await fullYearsCount(birthday);
   const alcoholic = fullYears >= 18 ? "" : "Non alcoholic";
   const query = {};
@@ -16,7 +17,10 @@ const getDrinksByFilters = async (req, res) => {
   drink && (query.drink = { $regex: drink, $options: "i" });
   alcoholic && (query.alcoholic = alcoholic);
 
-  const drinks = await Drink.find(query);
+  const drinks = await Drink.find(query, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
 
   if (!drinks) {
     throw HttpError(404, "Drink is not found");
@@ -26,7 +30,3 @@ const getDrinksByFilters = async (req, res) => {
 };
 
 module.exports = getDrinksByFilters;
-
-//   const pageNumber = parseInt(page) || 1;
-//   const limitNumber = parseInt(limit) || 10;
-//   const skip = (pageNumber - 1) * limitNumber;
