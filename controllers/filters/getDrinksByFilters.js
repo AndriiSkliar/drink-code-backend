8160063af55b46a6099dc7656f1aeb736db456fb
Андрей Скляр <andreas.skliar@gmail.com>
2024-02-29 15:23:43 +0200

    fix

--- controllers/filters/getDrinksByFilters.js
+++ controllers/filters/getDrinksByFilters.js
@@ -2,16 +2,20 @@ const { Drink } = require("../../models/drink");
 const { fullYearsCount } = require("../../helpers");
 
 const getDrinksByFilters = async (req, res) => {
-  const { drink, category, ingredient } = req.body;
-    const { birthday } = req.user;
+  const { drink, category, ingredient } = req.query;
+  const { birthday } = req.user;
   const fullYears = await fullYearsCount(birthday);
-  const isAdult = fullYears >= 18 ? "Alcoholic" : "Non alcoholic";
+  const isAdult = fullYears >= 18 ? "" : "Non alcoholic";
 
-  const categories = await Drink.find({})
-    .find(drink ? { $or: [{ drink: { $regex: drink, $options: "i" } }, { category: { $regex: drink, $options: "i" } }] } : {})
-    .find(category ? { category: { $regex: category, $options: "i" } } : {})
-    .find(ingredient ? { $or: [{ "ingredients.title": ingredient }] } : {})
-    .find(isAdult ? { alcoholic: {$regex: isAdult, $options: "i" } } : {});
+  const query = {};
+
+  if (drink) query.drink = { $regex: drink, $options: "i" };
+  if (category) query.category = { $regex: category, $options: "i" };
+  if (ingredient)
+    query["ingredients.title"] = { $regex: ingredient, $options: "i" };
+  query.alcoholic = { $regex: isAdult, $options: "i" };
+
+  const categories = await Drink.find(query);
 
   res.status(200).json(categories);
 };
